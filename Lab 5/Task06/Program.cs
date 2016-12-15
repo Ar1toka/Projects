bde4e891e6b03bcfef417932e4f07e8ebd0a70d5
Aritoka <htd@bk.ru>
2016-12-15 16:44:22 +0600

    12
    332

--- Lab 5/Task06/Program.cs
+++ Lab 5/Task06/Program.cs
@@ -10,6 +10,33 @@ namespace Task06
     {
         static void Main(string[] args)
         {
+            
+            Console.Write("Введите значение скорость V1 в километрах в час: ");
+            double a = double.Parse(Console.ReadLine());
+            if (a<0)
+            {
+                Console.WriteLine("Введите положительно число", a);
+                return;
+            }
+            Console.Write("Введите скорость V2 в метрах в секунду: ");
+            double b = double.Parse(Console.ReadLine());
+            if (b < 0)
+            {
+                Console.WriteLine("Введите положительно число", b);
+                return;
+            }
+            double d = (b*10)/36;
+            //double e = b;
+            if (d > a)
+            {
+                Console.WriteLine("V1 (км/ч) меньше V2 (м/с)");
+            }
+            else
+            {
+                Console.WriteLine("V2 (м/с) меньше V1(км/ч)");
+            }
+
+            Console.ReadLine();
         }
     }
 }
