f52d3dc94fccf2c9b2796f7318bc1f6e1bd84626
Aritoka <htd@bk.ru>
2016-12-17 15:23:14 +0600

    задание 7
    выполнено

--- Lab 5/Task07/Program.cs
+++ Lab 5/Task07/Program.cs
@@ -10,6 +10,34 @@ namespace Task07
     {
         static void Main(string[] args)
         {
+            const double PI = 3.1416;
+            beginning:
+            Console.Write("Введите радиус круга: ");
+            double a = double.Parse(Console.ReadLine());
+            if (a < 0)
+                {
+                    Console.WriteLine("Введите положительное число");
+                    goto beginning;
+                }
+            beginning2:
+            Console.Write("Введите сторону квадрата: ");
+            double b = double.Parse(Console.ReadLine());
+            if (b < 0)
+            {
+                Console.WriteLine("Введите положительное число");
+                goto beginning2;
+            }
+            double c = 2 * PI * a;
+            double d = 2 * b;
+            if (b > a)
+            {
+                Console.WriteLine("Площадь квадрата больше площади круга");
+            }
+            else
+            {
+                Console.WriteLine("Площадь круга больше площади квадрата");
+            }
+            Console.ReadLine();
         }
     }
 }
