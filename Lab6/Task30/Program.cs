ee61c44a2f34713d767921b0d561c414f40505e1
Aritoka <htd@bk.ru>
2017-01-31 13:06:56 +0600

    3d task finished
    Signed-off-by: Aritoka <htd@bk.ru>

--- Lab6/Task30/Program.cs
+++ Lab6/Task30/Program.cs
@@ -14,8 +14,7 @@ namespace Task30
             int b;
             int c = 1;
             int d = 0;
-            int e = 1;
-            int f =0;
+            int e = 0;
             int g = 0;
             Console.WriteLine("Введите значение A (0 - признак конца): ");
             b = int.Parse(Console.ReadLine());
@@ -32,14 +31,14 @@ namespace Task30
                 }
                 if (a<0 && a!=0)
                 { e = e + 1;
-                f = a;
+                if (e == 2) g = a;
                 }
-                if (e == 2) g = f;
+                
             }
             while (a != 0);
             Console.WriteLine("Максимальное число: " + b);
             Console.WriteLine("По счёту: " + d);
-            Console.WriteLine("Pyfxtybt dnjhjuj ytxtnyjuj: " + g);
+            Console.WriteLine("Значение второго отрицательного числа: " + g);
  
 
             Console.ReadLine();
