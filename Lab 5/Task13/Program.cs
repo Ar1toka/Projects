d90441eaa76cf730493ac65f85476a73b66cf729
Aritoka <htd@bk.ru>
2017-02-06 11:42:59 +0600

    несколько правок
    Signed-off-by: Aritoka <htd@bk.ru>

--- Lab 5/Task13/Program.cs
+++ Lab 5/Task13/Program.cs
@@ -16,28 +16,64 @@ namespace Task13
             double b = double.Parse(Console.ReadLine());
             Console.Write("Введите значение Z: ");
             double c = double.Parse(Console.ReadLine());
+            string d = Task13(a, b, c);
+            Console.WriteLine(d);
+            Console.WriteLine(Task13(6.35, 2.7, 5.08));
+            Console.WriteLine(Task13(4.71, 2.59, -18.55));
+            Console.WriteLine(Task13(19.99, -1.57, 2.75));
+            Console.WriteLine(Task13(-2.28, 16.92, 6.14));
 
-            double x=0;
-                double y=0;
-                double z = 0;
-                if ((a > -3) && (a < 5))
-                {
-                    x = a;
-                }
-            if ((a <= -3) || (a >= 5))
-            {
-
-            }
-            if ((c == b) || (b == a) || (c == a))
-            {
-                Console.WriteLine("Среди чисел есть равные");
-            }
-            if ((c == b) || (b == a) || (c == a))
-            {
-                Console.WriteLine("Среди чисел есть равные");
-            }
 
             Console.ReadLine();
+        }
+        public static string Task13(double a, double b, double c)
+        {
+            double x = 0;
+            double y = 0;
+            double z = 0;
+            if ((a > -3) && (a < 5))
+            {
+                x = a;
+
+            }
+
+            if ((b > -3) && (b < 5))
+            {
+                y = b;
+            }
+            if ((c > -3) && (c < 5))
+            {
+                z = c;
+            }
+            if (x != 0 && y != 0 && z != 0)
+            {
+                return x + " " + y + " " + z;
+            }
+            if (x != 0 && y != 0 && z == 0)
+            {
+                return x + " " + y;
+            }
+            if (x == 0 && y != 0 && z != 0)
+            {
+                return y + " " + z;
+            }
+            if (x != 0 && y == 0 && z != 0)
+            {
+                Console.WriteLine(x + " " + z);
+            }
+            if (x == 0 && y == 0 && z != 0)
+            {
+                return Convert.ToString(z);
+            }
+            if (x != 0 && y == 0 && z == 0)
+            {
+                return Convert.ToString(x);
+            }
+            if (x == 0 && y != 0 && z == 0)
+            {
+                return Convert.ToString(y);
+            }
+            else return "Введенные значения находятся за пределами интервала (-3;5)";
         }
     }
 }
--- Lab4/Task05/Program.cs
+++ Lab4/Task05/Program.cs
@@ -10,7 +10,67 @@ namespace Task05
     {
         static void Main(string[] args)
         {
+           double a1 = 0;
+             double   a2 = 0;
+             double   b1 = 0;
+              double      b2 = 0;
+            Console.WriteLine("Введите значение X1: ");
+            double x1 = double.Parse(Console.ReadLine());
+            //while (x1 <= 0)
+            //{
+            //    Console.WriteLine("Значение X1 должно быть больше 0");
+            //    Console.Write("Введите значение X1: ");
+            //    x1 = double.Parse(Console.ReadLine());
 
+
+            //}
+            Console.Write("Введите значение Y1: ");
+            double y1 = double.Parse(Console.ReadLine());
+            //while (y1 <= 0)
+            //{
+            //    Console.WriteLine("Значение Y1 должно быть больше 0");
+            //    Console.Write("Введите значение Y1: ");
+            //    y1 = double.Parse(Console.ReadLine());
+
+
+            //}
+            Console.WriteLine("Введите значение X2: ");
+            double x2 = double.Parse(Console.ReadLine());
+            //while (x2 <= 0)
+            //{
+            //    Console.WriteLine("Значение X2 должно быть больше 0");
+            //    Console.Write("Введите значение X2: ");
+            //    x2 = double.Parse(Console.ReadLine());
+
+
+            //}
+            Console.WriteLine("Введите значение Y2: ");
+            double y2 = double.Parse(Console.ReadLine());
+            //while (y2 <= 0)
+            //{
+            //    Console.WriteLine("Значение Y2 должно быть больше 0");
+            //    Console.Write("Введите значение Y2: ");
+            //    y2 = double.Parse(Console.ReadLine());
+
+
+            //}
+            Console.WriteLine("Введите значение a: ");
+            double a = double.Parse(Console.ReadLine());
+            //while (y2 <= 0)
+            //{
+            //    Console.WriteLine("Значение Y2 должно быть больше 0");
+            //    Console.Write("Введите значение Y2: ");
+            //    y2 = double.Parse(Console.ReadLine());
+
+
+            //}
+            
+            
+            
+            a1 = Math.Cos( a);
+            a2 = -Math.Sin(a);
+            b1 = Math.Sin( a);
+            b2 = Math.Cos( a);
         }
     }
 }
