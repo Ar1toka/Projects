7dc23ca46f297aa596a6ada993b67b6c29128d6f
Aritoka <htd@bk.ru>
2017-02-07 13:30:00 +0600

    пара заданий по двумерным массивам
    Signed-off-by: Aritoka <htd@bk.ru>

---
+++ Lab8/Task20/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            const int n = 4;
+            const int m = 4;
+            int[,]A=new int[n,m];
+            Random R = new Random();
+            for (int i=0 ; i<n ; i++)
+            { for (int j=0; j<m; j++)
+            {
+                A[i,j]=R.Next(20);
+                Console.Write(" "+A[i,j]);
+                if (i<j) s=s+A[i,j];
+            }
+            Console.WriteLine();
+            }
+            Console.WriteLine("Сумма: " + s);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab8/Task22/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            const int n = 4;
+            const int m = 6;
+            int[,] A = new int[n, m];
+            Random R = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    A[i, j] = R.Next(20);
+                    Console.Write(" " + A[i, j]);
+                    s = s + A[i, j];
+
+                }
+                //Console.WriteLine();
+                Console.WriteLine(" Сумма: " + s);
+            }
+            //Console.WriteLine("Сумма: " + s);
+            Console.ReadLine();
+        }
+    }
+}
