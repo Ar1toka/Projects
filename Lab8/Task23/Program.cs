7d018adf4d9f88534166bf15bd80ca41dafa4430
Aritoka <htd@bk.ru>
2017-02-09 01:02:00 +0600

    доделал транспонирование
    Signed-off-by: Aritoka <htd@bk.ru>

---
+++ Lab8/Task23/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 3;
+            const int m = 3;
+            int[,] A = new int[n, m];
+            Random R = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    A[i, j] = R.Next(20);
+                    Console.Write("\t " + A[i, j]);
+
+
+
+                }
+                Console.WriteLine();
+
+
+            }
+            Console.WriteLine();
+            int zam = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    zam = A[i, j];
+                    A[i, j] = A[j, i];
+                    A[j, i] = zam;
+
+                }
+                Console.WriteLine();
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("\t " + A[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
