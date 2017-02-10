566a1b4020be71787d3ffb209de9fca9b845e5e2
Aritoka <htd@bk.ru>
2017-02-10 11:57:49 +0600

    пара заданий по 7 лабе
    Signed-off-by: Aritoka <htd@bk.ru>

---
+++ Lab7/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение S(из представленных букв - abcdefwxyz): ");
+            string s = Console.ReadLine();
+            //char[] y1 = "abcdefwxyz".ToCharArray();
+            string source = "abcdefwxyz";
+             int index = source.IndexOf(s);
+             char[] y1 = source.ToCharArray();
+             int a = y1.Length;
+           int b = a-index-1;
+           Console.WriteLine("Количество букв после введенной вами: "+ b);
+           Console.ReadLine();
+        }
+    }
+}
---
+++ Lab7/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            string source = "abcdefwxyz";
+                    Console.WriteLine("Введите значение S(из представленных букв - abcdefwxyz): ");
+            string s = Console.ReadLine();
+            Console.WriteLine("Введите значение M(из представленных букв - abcdefwxyz): ");
+            string m = Console.ReadLine();
+            Console.WriteLine(string.Compare(s, source));
+            int index = source.IndexOf(s);
+            int index2 = source.IndexOf(m);
+            //char[] y1 = source.ToCharArray();
+            if (index < index2)
+ 
+            a = index2-index-1;
+            if (index > index2)
+            a = index-index2-1;
+            Console.WriteLine("Количество букв между введённых вами: " + a);
+            Console.ReadLine();
+        }
+    }
+}
